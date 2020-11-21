0d8cc9b96c3db0495270ca8278118fcd937de267
ArturShyshka <ashushka3@gmail.com>
2020-11-21 13:48:26 +0200

    task2

--- test.php
+++ test.php
@@ -1,33 +1,35 @@
 <?php
-$items = [
-	[
-		"title" = "Reports",
-		"tags" = "reports file"
-	],
-	[
-		"title" = "Analytics",
-		"tags" = "analytics graphs"
-	],
-	[
-		"title" = "Export",
-		"tags" = "export download"
-	],
-	[
-		"title" = "Storage",
-		"tags" = "storage"
-	]
-];
+// $items = [
+// 	[
+// 		"title" = "Reports",
+// 		"tags" = "reports file"
+// 	],
+// 	[
+// 		"title" = "Analytics",
+// 		"tags" = "analytics graphs"
+// 	],
+// 	[
+// 		"title" = "Export",
+// 		"tags" = "export download"
+// 	],
+// 	[
+// 		"title" = "Storage",
+// 		"tags" = "storage"
+// 	]
+// ];
 
 // output();
 
 
 // task 2
-// $h = ['Privacy', 'Cookies and other similar technologies'];
+$h = ['Privacy', 'Cookies and other similar technologies'];
+$p = ['Your privacy is important to us at SmartAdmin and the protection, confidentiality and integrity of your personal data are our prime concerns. We will only use your personal information to administer your account, provide the products and services you have requested from us, and to keep you informed about our products and services (if you have consented to this). We only use your data for the purposes for which it was collected and, where relevant, to meet local legal obligations. We will retain your personal information only for as long as is necessary for the purposes for which the information was collected, or as long as is required pursuant to law.', 'We collect certain data through cookies and similar technologies (e.g. web beacons, tags, device identifiers). Cookies are text files placed on your computer to collect standard internet log information and visitor behaviour information. This information is used to track visitor use of the website and to compile statistical reports on website activity. We register your interaction with our services in order to improve our website, content and services. Our use of such technologies and the data collected is described in more detail in our Cookie Policy. You can manage your cookie preferences through your browser settings.'];
+foreach (array_combine($p, $h) as $key) {
+	$a = '<h2>'.$h[0].'</h2><p class="mb-g">'. $p[0] .'</p><h3>'.$h[1].'</h3><p>'. $p[1] .'</p>';
+}
+echo $a;
 
-// $p = ['Your privacy is important to us at SmartAdmin and the protection, confidentiality and integrity of your personal data are our prime concerns.
-// We will only use your personal information to administer your account, provide the products and services you have requested from us, and to keep you informed about our products and services (if you have consented to this).
-// We only use your data for the purposes for which it was collected and, where relevant, to meet local legal obligations.
-// We will retain your personal information only for as long as is necessary for the purposes for which the information was collected, or as long as is required pursuant to law.', 'We collect certain data through cookies and similar technologies (e.g. web beacons, tags, device identifiers). Cookies are text files placed on your computer to collect standard internet log information and visitor behaviour information. This information is used to track visitor use of the website and to compile statistical reports on website activity. We register your interaction with our services in order to improve our website, content and services. Our use of such technologies and the data collected is described in more detail in our Cookie Policy. You can manage your cookie preferences through your browser settings.'];
+
 
 // foreach (array_combine($p, $h) as $key) {
 // 	$a = '<h2>'.$h[0].'</h2><p class="mb-g">'. $p[0] .'</p><h3>'.$h[1].'</h3><p>'. $p[1] .'</p>';
