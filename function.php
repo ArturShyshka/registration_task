150c81e0e1332d4c65ca87f8e1bbc2ec4c51c928
ArturShyshka <ashushka3@gmail.com>
2020-11-25 15:02:39 +0200

    registration

---
+++ function.php
@@ -0,0 +1,23 @@
+<?php 
+
+function get_user_by_email($email) {
+
+};
+
+function add_user($email, $password) {
+
+};
+
+function set_flash_message($name, $message) {
+
+};
+
+function display_flash_message($name) {
+
+};
+
+function redirect_to($path) {
+	
+};
+
+?>
